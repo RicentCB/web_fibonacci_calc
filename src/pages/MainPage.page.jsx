37e71cd1b2e375b3eb3d1ad4d42fb64f621de15c
Ricardo Cruz <ricardo.cb009@gmail.com>
2021-10-30 23:18:36 -0500

    [UP] Imagen de Fondo

--- src/pages/MainPage.page.jsx
+++ src/pages/MainPage.page.jsx
@@ -3,10 +3,12 @@ import {
   BackgroundContainer,
   MainContainer,
   BackgroundImage,
-  CardWrapper
+  CardWrapper,
+  CardContainer,
+  CardTitle
 } from './MainPage.styled'
 // Asets
-import BackImg from '../assets/goldenRatioBig.jpg';
+import BackImg from '../assets/spiral.jpg';
 
 const MainPage = () => {
   return (
@@ -15,7 +17,11 @@ const MainPage = () => {
           <BackgroundImage src={BackImg}/>
         </BackgroundContainer>
         <CardWrapper>
-          ''
+          <CardContainer>
+            <CardTitle>
+              Calculadora de Fibonacci
+            </CardTitle>
+          </CardContainer>
         </CardWrapper>
       </MainContainer>
   )
